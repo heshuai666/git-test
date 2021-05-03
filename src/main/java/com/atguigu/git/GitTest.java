20ce96f6588d82b72eb84d7c66e26a1bdaebbeee
heshuai666 <49748696+heshuai666@users.noreply.github.com>
2021-05-03 20:08:02 +0800

    Update GitTest.java
    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git44!");
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
-        System.out.println("hello pull!");
+        System.out.println("hello pull1!");
     }
 }
